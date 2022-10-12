75e1dc4fbb68759f741de7eff8f513a7b09d0703
ThomBurt <93936296+ThomBurt@users.noreply.github.com>
2022-10-12 08:53:07 -0400

    google sheets:

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const htmlRoutes = require('./routes/htmlRoute');
+const { google } = require("googleapis");
 
 const PORT = process.env.PORT || 3001;
 
@@ -19,13 +20,61 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Set ups the HTML directory path for startup
-app.get('/', (req, res) =>
+app.get('/', async (req, res) => 
   res.sendFile(path.join(__dirname, '/public'))
 );
 
 
 //app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
+
+
+//Google Post
+
+app.post("/", async (req, res) => {
+
+  const { name, email } = req.body;
+
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "google-api-credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+  // Create client instance for auth
+  const client = await auth.getClient();
+
+  // Instance of Google Sheets API
+  const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  const spreadsheetId = "1lO9_KnDG9QOzRy9soRiam1rDQpNdyrWFJf21OH9HLd4";
+
+  // Get metadata about spreadsheet
+  const metaData = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId,
+  });
+
+  // Read rows from spreadsheet
+  const getRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "Sheet1",
+  });
+
+  // Write row(s) to spreadsheet
+  await googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: "Sheet1!A:B",
+    valueInputOption: "USER_ENTERED",
+    resource: {
+      values: [[name, email]],
+    },
+  });
+
+  res.send("Successfully submitted! Thank you!");
+});
+
 
 
 // Listening on PORT

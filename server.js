8866b4402038bdbf2bf42153f89c19e5ba25f2ea
ThomBurt <93936296+ThomBurt@users.noreply.github.com>
2022-11-05 16:18:51 -0400

    update

--- server.js
+++ server.js
@@ -35,61 +35,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 // parse application/json
 app.use(express.json());
 
-//Google Post
-app.post("/", async (req, res) => {
-
-  const { name, email } = req.body;
-
-  const auth = new google.auth.GoogleAuth({
-    // keyFile: "google-api-credentials.json",
-    credentials: process.env,
-    scopes: "https://www.googleapis.com/auth/spreadsheets",
-  });
-
-  // Create client instance for auth
-  const client = await auth.getClient();
-
-  // Instance of Google Sheets API
-  const googleSheets = google.sheets({ version: "v4", auth: client });
-
-  const spreadsheetId = "1lO9_KnDG9QOzRy9soRiam1rDQpNdyrWFJf21OH9HLd4";
-
-  // Get metadata about spreadsheet
-  const metaData = await googleSheets.spreadsheets.get({
-    auth,
-    spreadsheetId,
-  });
-
-  // Read rows from spreadsheet
-  const getRows = await googleSheets.spreadsheets.values.get({
-    auth,
-    spreadsheetId,
-    range: "Sheet1",
-  });
-
-  // Write row(s) to spreadsheet
-  await googleSheets.spreadsheets.values.append({
-    auth,
-    spreadsheetId,
-    range: "Sheet1!A:B",
-    valueInputOption: "USER_ENTERED",
-    resource: {
-      values: [[name, email]],
-    },
-  });
-  console.log("Email Signup successfully submitted, Thank you!")
-  res.send("Successfully submitted! Thank you!");
-
-//  function successMessage(){
-//   let div = document.createElement("div");
-//   div.setAttribute("col-md-12", "p2", "m-3", "bg-success", "rounded");
-//   let heading4 = document.createElement("h4");
-//   heading4.setAttribute("text-white");
-//   heading4.innerHTML("Email Signup Successful")
-//   div.appendChild(heading4)
-//  }
-//  successMessage();
-});
 
 
 //Sets up data parsing

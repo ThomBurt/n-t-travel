ea87766138ab678a6034c9716b2f2fd31e63bf1f
ThomBurt <93936296+ThomBurt@users.noreply.github.com>
2022-11-07 11:41:06 -0500

    fixed links

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -11,7 +11,7 @@ router.get('/about', (req, res) => {
   });
 
 router.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/contact.html'));
+    res.sendFile(path.join(__dirname, '../public/contactUs.html'));
   });
 
 router.get('/services', (req, res) => {
@@ -19,7 +19,31 @@ router.get('/services', (req, res) => {
   });
 
   router.get('/blog', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/services.html'));
+    res.sendFile(path.join(__dirname, '../public/travelBlog.html'));
+  });
+  router.get('/europe', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/europe.html'));
+  });
+  router.get('/north-america', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/northAmerica.html'));
+  });
+  router.get('/central-and-south-america', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/centralAmerica.html'));
+  });
+  router.get('/oceania', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/oceania.html'));
+  });
+
+  router.get('/careers', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/jobs.html'));
+  });
+
+  router.get('/newsletter', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/newsletter.html'));
+  });
+
+  router.get('/travel-packing-list', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/travelPackingList.html'));
   });
 
 router.get('*', (req, res) => {
